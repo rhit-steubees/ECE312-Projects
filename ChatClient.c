85d6b1a1ef5b3cc12b868d493ec28a46d08d7325
rhit_steubees <steubees@rose-hulman.edu>
2025-09-21 16:08:01 -0400

    small fix

--- ChatClient.c
+++ ChatClient.c
@@ -14,23 +14,26 @@
 
    int main(int argc, char *argv[])
    {
-     int sockfd, portno, n;
-     struct sockaddr_in serv_addr;
-     struct hostent *server;
-     char buffer[256];
-     if (argc < 3) {
-        fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    
+    int sockfd, portno, n;
+    struct sockaddr_in serv_addr;
+    struct hostent *server;
+    char buffer[256];
+    char quit[256] = "quit\n";
+
+    if (argc < 3) {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    exit(0);
+    }
+    portno = atoi(argv[2]);
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) 
+        error("ERROR opening socket");
+    server = gethostbyname(argv[1]);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
         exit(0);
-     }
-     portno = atoi(argv[2]);
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
-     if (sockfd < 0) 
-         error("ERROR opening socket");
-     server = gethostbyname(argv[1]);
-     if (server == NULL) {
-         fprintf(stderr,"ERROR, no such host\n");
-         exit(0);
-     }
+    }
 
     //  bzero((char *) &serv_addr, sizeof(serv_addr));
     memset(&serv_addr, 0, sizeof(serv_addr)); // clear server address
